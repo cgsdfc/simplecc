b00829b46fa81f416afa65e7a7c7363691bfb36d
cgsdfc <cgsdfc@126.com>
2018-11-09 23:21:01 +0800

    Add script to test cst

---
+++ test_cst.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+"""Cross-validate cst generation"""
+
+import os
+import tempfile
+
+def run_test(config):
+    cparser = os.path.abspath(config['cparser'])
+    pyparser = os.path.abspath(config['pyparser'])
+    input = os.path.abspath(config['input'])
+
+    cparser_out = tempfile.mkstemp(prefix='cpp')[1]
+    pyparser_out = tempfile.mkstemp(prefix='py')[1]
+
+    cparser = "{exe} {input} >{tmp}".format(exe=cparser, input=input, tmp=cparser_out)
+    pyparser = "{exe} {input} -r >{tmp}".format(exe=pyparser, input=input, tmp=pyparser_out)
+    os.system(cparser)
+    os.system(pyparser)
+    os.system("cmp {a} {b}".format(a=cparser_out, b=pyparser_out))
+
+    os.remove(cparser_out)
+    os.remove(pyparser_out)
+
+
+def main():
+    import argparse
+    import json
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', dest='config', type=argparse.FileType(),
+            help='configure file', required=1)
+    args = parser.parse_args()
+
+    run_test(json.load(args.config))
+
+if __name__ == '__main__':
+    main()
