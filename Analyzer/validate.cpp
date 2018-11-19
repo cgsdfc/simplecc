9895633b24e8de0f45671b613560c6822a5b8e81
cgsdfc <cgsdfc@126.com>
2018-11-19 18:44:29 +0800

    Update SyntaxValidator to use VisitorBase and ErrorManager
    This is part of an act to streamline visitors

--- Analyzer/validate.cpp
+++ Analyzer/validate.cpp
@@ -1,121 +1,97 @@
 #include "validate.h"
 #include "error.h"
 
-class SyntaxValidator {
+class SyntaxValidator: public VisitorBase<SyntaxValidator> {
+  ErrorManager e;
 public:
 
-  bool visit(Program *node) {
+  SyntaxValidator(): VisitorBase(), e() {}
+
+  void visitProgram(Program *node) {
     if (node->decls.size() == 0) {
-      Error(Location(0, 0), "expected main() at the end of input");
-      return false;
+      e.Error(Location(0, 0), "expected main() at the end of input");
+      return;
     }
-    auto child_ok = visit(node->decls);
+    for (auto decl: node->decls) {
+      VisitorBase::visit<void>(decl);
+    }
+
     auto decl_iter = node->decls.begin();
     auto end = node->decls.end();
 
     // check the order of declarations
-    while (decl_iter != end && IsInstance<ConstDecl>(*decl_iter))
+    while (decl_iter != end && IsInstance<ConstDecl>(*decl_iter)) {
       ++decl_iter;
-    while (decl_iter != end && IsInstance<VarDecl>(*decl_iter))
+    }
+    while (decl_iter != end && IsInstance<VarDecl>(*decl_iter)) {
       ++decl_iter;
-    while (decl_iter != end && IsInstance<FuncDef>(*decl_iter))
+    }
+    while (decl_iter != end && IsInstance<FuncDef>(*decl_iter)) {
       ++decl_iter;
+    }
 
     if (decl_iter != end) {
       auto decl = *decl_iter;
-      auto loc = decl->loc;
-      Error(loc, "unexpected", decl->ClassName(), Quote(GetDeclName(decl)));
-      return false;
+      e.Error(decl->loc, "unexpected", decl->ClassName(), Quote(GetDeclName(decl)));
     }
 
     // check the last declaration is the main function
     auto x = subclass_cast<FuncDef>(node->decls.back());
     if (!(x && x->name == "main")) {
-      Error(x->loc, "expected main() at the end of input");
-      return false;
+      e.Error(x->loc, "expected main() at the end of input");
     }
-
-    return child_ok;
   }
 
-  bool visit(ConstDecl *node) {
-    if (node->type == BasicTypeKind::Int) {
-      if (IsInstance<Num>(node->value)) {
-        return true;
-      }
-      Error(node->loc, "const int", Quote(node->name), "expects an integer");
-      return false;
+  void visitConstDecl(ConstDecl *node) {
+    if (node->type == BasicTypeKind::Int && !IsInstance<Num>(node->value)) {
+      e.Error(node->loc, "const int", Quote(node->name), "expects an integer");
     }
     else {
       assert(node->type == BasicTypeKind::Character);
-      if (IsInstance<Char>(node->value)) {
-        return true;
+      if (!IsInstance<Char>(node->value)) {
+        e.Error(node->loc, "cont char", Quote(node->name), "expects a character");
       }
-      Error(node->loc, "cont char", Quote(node->name), "expects a character");
-      return false;
     }
   }
 
-  bool visit(Decl *node) {
-    if (auto x = subclass_cast<ConstDecl>(node))
-      return visit(x);
-    else if (auto x = subclass_cast<VarDecl>(node))
-      return visit(x);
-    else {
-      auto y = subclass_cast<FuncDef>(node);
-      assert(y);
-      return visit(y);
-    }
-  }
-
-  bool visit(VarDecl *node) {
+  void visitVarDecl(VarDecl *node) {
     if (node->type == BasicTypeKind::Void) {
-      Error(node->loc, "cannot declare", Quote(node->name), "as a void variable");
-      return false;
+      e.Error(node->loc, "cannot declare", Quote(node->name), "as a void variable");
     }
-    if (node->is_array && node->size == 0) {
-      Error(node->loc, "array size of", Quote(node->name), "cannot be 0");
-      return false;
+    else if (node->is_array && node->size == 0) {
+      e.Error(node->loc, "array size of", Quote(node->name), "cannot be 0");
     }
-    return true;
   }
 
-  bool visit(Arg *node, const String &funname) {
+  void visitArg(Arg *node, const String &funname) {
     if (node->type == BasicTypeKind::Void) {
-      Error(node->loc, "cannot declare void argument", Quote(node->name),
+      e.Error(node->loc, "cannot declare void argument", Quote(node->name),
           "of function", Quote(funname));
-      return false;
     }
-    return true;
   }
 
-  template<typename T, typename... A>
-    bool visit(const std::vector<T> &list, A&&... args) {
-      bool ok = true;
-      for (auto child: list) {
-        if (!visit(child, args...)) {
-          ok = false;
-        }
-      }
-      return ok;
+  void visitFuncDef(FuncDef *node) {
+    for (auto arg: node->args) {
+      visitArg(arg, node->name);
     }
-
-  bool visit(FuncDef *node) {
-    auto args_ok = visit(node->args, node->name);
-    auto decls_ok = visit(node->decls);
+    for (auto decl: node->decls) {
+      VisitorBase::visit<void>(decl);
+    }
 
     if (node->name == "main") {
       if (node->return_type != BasicTypeKind::Void) {
-        Error(node->loc, "main() must return void");
-        return false;
+        e.Error(node->loc, "main() must return void");
       }
     }
+  }
 
-    return args_ok && decls_ok;
+  bool Validate(Program *node) {
+    visitProgram(node);
+    return e.IsOk();
   }
 
 };
 
 bool ValidateSyntax(Program *node) {
-  return SyntaxValidator().visit(node);
+  return SyntaxValidator().Validate(node);
 }
