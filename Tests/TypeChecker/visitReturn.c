2a72795ada3ecf0a32c82cedfef7185d70d21725
cgsdfc <cgsdfc@126.com>
2018-11-21 19:02:24 +0800

    Test visitReturn
    Rename test folder to match the class under test

---
+++ Tests/TypeChecker/visitReturn.c
@@ -0,0 +1,48 @@
+int i_fun1 {
+  return;
+}
+
+int i_fun2 {
+  char c;
+  return (c);
+}
+
+char c_fun1 {
+  return;
+}
+
+char c_fun2 {
+  int i;
+  return (i);
+}
+
+void v_fun1 {
+  return (1);
+}
+
+void v_fun2 {
+  return ('c');
+}
+
+int i_fun3 {
+  return (1);
+}
+
+int i_fun4 {
+  return (('a'));
+}
+
+char c_fun3 {
+  return ('a');
+}
+
+char c_fun4 {
+  const char cconst = 'a';
+  return (cconst);
+}
+
+void v_fun3 {
+  return;
+}
+
+void main() {}
