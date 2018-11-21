da55b3279ab0a070277aba9964933b70e79356e4
cgsdfc <cgsdfc@126.com>
2018-11-21 20:56:33 +0800

    visitVarDecl

---
+++ Tests/SyntaxChecker/visitVarDecl.c
@@ -0,0 +1,16 @@
+void v1;
+void varr1[1];
+void v2, v3;
+
+int iarr1[0];
+char carr1[0];
+
+int v4, iarr2[0];
+char v5, carr2[0];
+
+int ival;
+char cval;
+int iarr3[1];
+char carr3[1];
+
+void main() {}
