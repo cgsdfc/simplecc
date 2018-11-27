1cc9b201f03f8a22790a8457e49e10dbbd5b0aac
cgsdfc <cgsdfc@126.com>
2018-11-27 13:33:47 +0800

    compile: Simpify visitIf and visitWhile tests

--- Tests/FunctionCompiler/visitIf.c
+++ Tests/FunctionCompiler/visitIf.c
@@ -12,22 +12,4 @@ void main() {
     ivar = 2;
   }
 
-  if (1) {
-    if (1) {
-      ivar = 1;
-    }
-  }
-
-  if (1) {
-    if (1) {
-      ivar = 1;
-    }
-    else {
-      ivar = 2;
-    }
-  }
-  else {
-    ivar = 2;
-  }
-
 }
--- Tests/FunctionCompiler/visitWhile.c
+++ Tests/FunctionCompiler/visitWhile.c
@@ -4,16 +4,9 @@ void main() {
   while (1) {
     printf(1);
   }
-  printf(1);
 
   while (ivar < 1) {
-    printf("ivar", ivar);
+    printf("ivar");
   }
-  printf(1);
-
-  while (1 > 1) {
-    printf(1);
-  }
-  printf(1);
 
 }
