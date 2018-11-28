b0ff1bdc0257e1246a941ba86c233b96c577b56d
cgsdfc <cgsdfc@126.com>
2018-11-28 13:10:33 +0800

    CLI: input => positional, output => -o
    both are optional and default to stdin/stdout

--- Codegen/main.cpp
+++ Codegen/main.cpp
@@ -14,23 +14,14 @@
 
 using namespace TCLAP;
 
-void TestSymbolTable(SymbolTable &symtable);
-
 
 int main(int argc, char **argv) {
   CmdLine parser("Simple Compiler Debugging helper", ' ', "0.0.1");
 
-  ValueArg<String> input_arg("i", "input",
-      "input file to compile", // description
-      true, // required
-      "", // default
-      "file", // metavar
-      parser);
-
   SwitchArg tokenize("", "tokenize", "break input into tokens", false);
   SwitchArg build_cst("", "build-cst", "run the parser and construct a concrete syntax tree", false);
   SwitchArg build_ast("", "build-ast", "create an abstract syntax tree from the concrete syntax tree", false);
-  SwitchArg syntax_check("", "syntax-check", "verify that the AST is syntax correct", false);
+  SwitchArg syntax_check("", "syntax-check", "verify that the AST is syntactically correct", false);
   SwitchArg build_symtable("", "build-symtable", "build a symbol table from the abstract syntax tree", false);
   SwitchArg type_check("", "type-check", "run type-check for the input program", false);
   SwitchArg print_bytecode("", "print-bytecode", "print byte code in quarternary form", false);
@@ -45,6 +36,13 @@ int main(int argc, char **argv) {
 
   parser.xorAdd(xor_list);
 
+  // Positional argument: input
+  UnlabeledValueArg<String> input_arg("input",
+      "input file (default to stdin)", false, "", "input-file", parser);
+
+  ValueArg<String> output_arg("o", "output",
+      "output file (default to stdout)", false, "", "output-file", parser);
+
   try {
     parser.parse(argc, argv);
   } catch (TCLAP::ArgException &e) {
@@ -53,17 +51,44 @@ int main(int argc, char **argv) {
   }
 
   // Prepare input stream
-  std::ifstream input_file(input_arg.getValue());
-  if (input_file.fail()) {
-    Print(std::cerr, "Error: File",
-        Quote(input_arg.getValue()), "does not exist");
-    return 1;
+  std::istream *input_stream;
+  std::ifstream  input_file;
+
+  if (!input_arg.isSet()) {
+    input_stream = &std::cin;
+  }
+  else {
+    input_file.open(input_arg.getValue());
+    if (input_file.fail()) {
+      Print(std::cerr, "Error: File",
+          Quote(input_arg.getValue()), "does not exist");
+      return 1;
+    }
+    input_stream = &input_file;
   }
 
+  // Prepare output stream
+  std::ostream *output_stream;
+  std::ofstream output_file;
+
+  if (!output_arg.isSet()) {
+    output_stream = &std::cout;
+  }
+  else {
+    output_file.open(output_arg.getValue());
+    if (output_file.fail()) {
+      Print(std::cerr, "Error: File",
+          Quote(output_arg.getValue()), "cannot be written to");
+      return 1;
+    }
+    output_stream = &output_file;
+  }
+
+
   TokenBuffer tokens;
-  Tokenize(input_file, tokens);
+  Tokenize(*input_stream, tokens);
   if (tokenize.getValue()) {
-    PrintTokens(tokens, std::cout);
+    PrintTokens(tokens, *output_stream);
     return 0;
   }
 
@@ -73,7 +98,7 @@ int main(int argc, char **argv) {
     return 1;
   }
   if (build_cst.getValue()) {
-    std::cout << *cst_node << "\n";
+    *output_stream << *cst_node << "\n";
     return 0;
   }
 
@@ -83,7 +108,7 @@ int main(int argc, char **argv) {
     return 1;
   }
   if (build_ast.getValue()) {
-    std::cout << *ast_node << "\n";
+    *output_stream << *ast_node << "\n";
     return 0;
   }
 
@@ -100,7 +125,7 @@ int main(int argc, char **argv) {
   }
   symtable.Check();
   if (build_symtable.getValue()) {
-    std::cout << symtable << "\n";
+    *output_stream << symtable << "\n";
     return 0;
   }
 
@@ -109,23 +134,23 @@ int main(int argc, char **argv) {
   }
   if (type_check.getValue()) {
     // Add expr_types information
-    std::cout << symtable << "\n";
+    *output_stream << symtable << "\n";
     return 0;
   }
 
   if (print_bytecode.getValue()) {
-    PrintByteCode(ast_node.get(), std::cout);
+    PrintByteCode(ast_node.get(), *output_stream);
     return 0;
   }
 
   auto &&module = CompileProgram(ast_node.get(), symtable);
   if (compile.getValue()) {
-    std::cout << module << "\n";
+    *output_stream << module << "\n";
     return 0;
   }
 
   if (assemble.getValue()) {
-    AssembleMips(module, std::cout);
+    AssembleMips(module, *output_stream);
     return 0;
   }
 
