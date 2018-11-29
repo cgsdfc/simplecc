57cf64be15fff0715b8f69adaad3fca0563f5a7f
cgsdfc <cgsdfc@126.com>
2018-11-29 10:56:27 +0800

    Fix PRINT_XXX & READ_XXX

---
+++ Tests/Assembler/Translation/PRINT_XXX.c
@@ -0,0 +1,5 @@
+void main() {
+  printf(1);
+  printf('a');
+  printf("string");
+}
---
+++ Tests/Assembler/Translation/READ_XXX.c
@@ -0,0 +1,13 @@
+void main() {
+  int IntVariable;
+  char CharVariable;
+
+  printf("Please input an integer and a character:");
+  scanf(IntVariable);
+  scanf(CharVariable);
+
+  printf("");
+  printf("Your input are:");
+  printf("Int: ", IntVariable);
+  printf("Char: ", CharVariable);
+}
