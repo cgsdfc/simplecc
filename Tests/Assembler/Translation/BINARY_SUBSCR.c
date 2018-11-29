0c302044ad42dc5604083a112c8010ff9b25ccc1
cgsdfc <cgsdfc@126.com>
2018-11-29 11:07:49 +0800

    Fix XXX_SUBSCR

---
+++ Tests/Assembler/Translation/BINARY_SUBSCR.c
@@ -0,0 +1,8 @@
+void main() {
+  int IntArray[2];
+  IntArray[0] = 1;
+  IntArray[1] = 2;
+
+  printf("at 0: ", IntArray[0]);
+  printf("at 1: ", IntArray[1]);
+}
---
+++ Tests/Assembler/Translation/STORE_SUBSCR.c
@@ -0,0 +1,9 @@
+int IntArray[2];
+
+void main() {
+  IntArray[0] = 1;
+  IntArray[1] = 2;
+
+  printf("at 0: ", IntArray[0]);
+  printf("at 1: ", IntArray[1]);
+}
