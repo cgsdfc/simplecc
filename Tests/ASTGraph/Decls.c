54e7029b1fd8e487ad1a969ec1a2ea645ad3dcde
cgsdfc <cgsdfc@126.com>
2018-12-04 14:30:42 +0800

    Add examples for ASTGraph

---
+++ Tests/ASTGraph/Decls.c
@@ -0,0 +1,6 @@
+const int IntConstant = 1;
+int Array[2];
+
+void main() {
+  Printf("hello");
+}
---
+++ Tests/ASTGraph/For.c
@@ -0,0 +1,6 @@
+void main() {
+  int i;
+  for (i = 0; i < 4; i = i + 1) {
+    Printf("hello");
+  }
+}
