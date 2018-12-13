30ec31d56fabda18137e5d51814f000be50816fe
cgsdfc <cgsdfc@126.com>
2018-12-13 11:36:39 +0800

    More Assemler/Mechanism Tests

---
+++ Tests/EmitLLVM/Iterative.c
@@ -0,0 +1,30 @@
+int IterativeFactoral(int Order) {
+  int Product;
+  int I;
+
+  if (Order < 0) {
+    printf("Invalid Order: ", Order);
+    return(-1);
+  }
+
+  if (Order == 0) return(1);
+
+  Product = 1;
+  I = 1;
+  while (I <= Order) {
+    Product = Product * I;
+    I = I + 1;
+  }
+  return(Product);
+}
+
+void main() {
+  Printf("Expect -1: ", IterativeFactoral(-1));
+  Printf("Expect 1: ", IterativeFactoral(0));
+  Printf("Expect 1: ", IterativeFactoral(1));
+  Printf("Expect 2: ", IterativeFactoral(2));
+  Printf("Expect 6: ", IterativeFactoral(3));
+  Printf("Expect 24: ", IterativeFactoral(4));
+  Printf("Expect 120: ", IterativeFactoral(5));
+  Printf("Expect 720: ", IterativeFactoral(6));
+}
--- Tests/EmitLLVM/visitFor.c
+++ Tests/EmitLLVM/visitFor.c
@@ -2,7 +2,7 @@ void main() {
   int i;
 
   for (i = 0; i < 3; i = i + 1) {
-    i = 1;
+    Printf("i: ", i);
   }
 
 }
