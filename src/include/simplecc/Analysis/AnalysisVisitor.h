5a689d5052b98bb3ed12770e18f3b4881407f24a
cgsdfc <cgsdfc@126.com>
2018-12-28 17:37:01 +0800

    Add doc.

--- src/include/simplecc/Analysis/AnalysisVisitor.h
+++ src/include/simplecc/Analysis/AnalysisVisitor.h
@@ -8,13 +8,21 @@
 
 namespace simplecc {
 /// This class is a CRTP base for classes that perform analysis on the AST.
-/// subclasses are **allowed** to mutate both the AST and the SymbolTable.
+/// It sets up the corresponding LocalSymbolTable for each FuncDef to be visit.
+/// It also acts as an ErrorManger so that subclass don't need to own one.
+/// It provides a Check() method that means to streamline the interface of subclasses.
+/// Due to CRTP, subclasses should
+/// 1. private inherit this class.
+/// 2. make friends with all its Visitor-like base classes. (Currently 3).
+/// 3. mark Check() as public.
+/// Note: subclasses are **allowed** to mutate both the AST and the SymbolTable.
 template<typename Derived>
 class AnalysisVisitor : public ChildrenVisitor<Derived>, public ErrorManager {
 public:
   AnalysisVisitor(const char *ErrorType = nullptr)
       : ErrorManager(ErrorType), TheTable(), TheLocalTable() {}
 
+  /// Public interface. Subclass should mark it as public.
   bool Check(Program *P, SymbolTable &S) {
     setTable(S);
     static_cast<Derived *>(this)->visitProgram(P);
@@ -22,6 +30,7 @@ public:
   }
 
 protected:
+  /// Access to the SymbolTable.
   void setTable(SymbolTable &S) {
     TheTable = &S;
   }
@@ -34,6 +43,7 @@ protected:
     return *TheTable;
   }
 
+  /// Access to the LocalSymbolTable per FuncDef basic.
   void setLocalTable(FuncDef *FD) {
     TheLocalTable = getSymbolTable().getLocalTable(FD);
   }
@@ -44,6 +54,8 @@ protected:
     return TheLocalTable[Name];
   }
 
+  /// Initialize LocalSymbolTable before performing real visit.
+  /// Subclass can override it and add more initialization.
   void visitFuncDef(FuncDef *FD) {
     setLocalTable(FD);
     ChildrenVisitor<Derived>::visitFuncDef(FD);
