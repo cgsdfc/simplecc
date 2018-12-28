0a0ca811ae18f5294795aa1ab7396089011706f4
cgsdfc <cgsdfc@126.com>
2018-12-28 11:48:20 +0800

    Cleanup AstRef.
    It is an adapter from AST to llvm::WriteGraph().

--- src/include/simplecc/Visualize/AstRef.h
+++ src/include/simplecc/Visualize/AstRef.h
@@ -1,36 +1,24 @@
 #ifndef SIMPLECC_VISUALIZE_ASTREF_H
 #define SIMPLECC_VISUALIZE_ASTREF_H
-#include "simplecc/Parse/AST.h"
 
 namespace simplecc {
 class AstGraph;
+class AST;
 
-/// A wrapper around AST* that records its real type.
-/// Stand in place of A cheap reference to Ast Nodes.
+/// A wrapper class that binds an AstGraph to an AST ptr.
 class AstRef {
-  friend class AstGraph;
   AST *Ref;
   /// Pointer to enclosing AstGraph. This is required since
   /// GraphTraits::child_XXX() takes **only** a NodeRef.
   AstGraph *Parent;
-
 public:
-  /// Construct from one of the known subclasses of AST.
-  /// Only AstGraph is allowed to create it.
-  AstRef(AST *Ptr, AstGraph *P)
-      : Ref(Ptr), Parent(P) {}
+  AstRef(AST *Ptr, AstGraph *P) : Ref(Ptr), Parent(P) {}
 
-  /// Construct an empty AstRef, like nullptr to AST*.
-  AstRef() : Ref(nullptr), Parent() {}
-
-  bool operator==(const AstRef &O) const { return Ref == O.Ref; }
-
-  /// get() without a type argument returns the **raw** AST pointer.
   AST *get() const { return Ref; }
+  AstGraph *getParent() const { return Parent; }
 
   const char *getClassName() const { return Ref->getClassName(); };
   Location getLocation() const { return Ref->getLocation(); }
-  AstGraph *getParent() const { return Parent; }
 };
 } // namespace simplecc
 
