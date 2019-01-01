615c265b62033b293d7dc83f419fc8ffcce25f9d
cgsdfc <cgsdfc@126.com>
2019-01-01 23:23:36 +0800

    Remove InlineConstDeclTransformer

--- src/lib/Transform/Transform.cpp
+++ src/lib/Transform/Transform.cpp
@@ -1,11 +1,9 @@
 #include "simplecc/Transform/Transform.h"
-#include "simplecc/Transform/InlineConstDeclTransformer.h"
 #include "simplecc/Transform/TrivialConstantFolder.h"
 #include "simplecc/Transform/DeadCodeEliminator.h"
 
 namespace simplecc {
 void TransformProgram(Program *P, SymbolTable &S) {
-  InlineConstDeclTransformer().Transform(P, S);
   TrivialConstantFolder().Transform(P, S);
   DeadCodeEliminator().Transform(P);
 }
