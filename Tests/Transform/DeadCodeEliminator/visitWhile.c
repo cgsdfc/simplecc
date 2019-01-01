87df242b61d2d05157068bce60d6157ddafd2763
cgsdfc <cgsdfc@126.com>
2019-01-01 21:12:33 +0800

    Fix visitWhile.

--- Tests/Transform/DeadCodeEliminator/visitWhile.c
+++ Tests/Transform/DeadCodeEliminator/visitWhile.c
@@ -1,6 +1,28 @@
+int X;
+
 void deleteIfConditionIsFalse(int a) {
   while (0) {
     printf(1);
+  }
+}
+
+void withinStmtList(int a) {
+  printf(1);
+  while (0) {
+    X = 1;
+  }
+  printf(2);
+}
+
+void NeedConstFold(int a) {
+  while (X - X) {
+    printf(1);
+  }
+}
+
+void NoOptimize(int a) {
+  while (X) {
+    X = 1;
   }
 }
 
