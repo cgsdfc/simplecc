478982c0a3b203212cc823d168fb1e5911e03d51
cgsdfc <cgsdfc@126.com>
2019-01-05 11:43:36 +0800

    Fix CMake, remove windows_main.cp

--- src/lib/Driver/windows_main.cpp
+++
@@ -1,8 +0,0 @@
-#include "simplecc/Driver/WindowsDriver.h"
-
-using namespace simplecc;
-
-int main(int argc, char **argv) {
-  std::unique_ptr<WindowsDriver> TheDriver(new WindowsDriver());
-  return TheDriver->run(argc, argv);
-}
